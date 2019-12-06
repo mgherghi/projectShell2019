e2ae9186068511b4595210707c12b974f448881e
Mihai Gherghina <m@Mihais-MacBook-Pro.local>
2019-12-05 19:52:20 -0800

    again

---
+++ api/entities/category.js
@@ -0,0 +1,20 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+  } from 'typeorm';
+  import User from './user';
+  
+  @Entity()
+  export default class Category {
+    @PrimaryGeneratedColumn()
+    id
+  
+    @Column({ type: 'varchar' })
+    name
+  
+    @ManyToOne(() => User, (user) => user.categories)
+    user
+  }
+  
---
+++ api/entities/todo.js
@@ -0,0 +1,27 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    ManyToOne,
+  } from 'typeorm';
+  import User from './user';
+  import Category from './category';
+  
+  @Entity()
+  export default class ToDo {
+    @PrimaryGeneratedColumn()
+    id
+  
+    @Column({ type: 'boolean' })
+    done
+  
+    @Column({ type: 'varchar' })
+    title
+  
+    @ManyToOne(() => User, (user) => user.todos)
+    user
+  
+    @ManyToOne(() => Category, (category) => category.todos)
+    category
+  }
+  
